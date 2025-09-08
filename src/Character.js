4121dd9462da6b0b3ddb57b2abdb121164cef8bb
coffeebiscuits <opopkl-@nate.com>
2025-09-08 20:30:22 +0900

    Update Character.js

--- src/Character.js
+++ src/Character.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Character.css";
 
-const CharacterPage = () => {
+const Character = () => {
   const [text, setText] = useState("");
 
   useEffect(() => {
