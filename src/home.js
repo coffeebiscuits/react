a7361b18f4c165693314f3b1890e82815cafdd4e
coffeebiscuits <opopkl-@nate.com>
2025-09-09 21:05:35 +0900

    Create home.js

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>홈 페이지 테스트</h1>
+      <p>버튼을 눌러 이동 성공!</p>
+    </div>
+  );
+};
+
+export default Home;
