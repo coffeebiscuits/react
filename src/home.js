a46ab58ae2f4f492d0fa3862494b41e0d8cf0a22
coffeebiscuits <opopkl-@nate.com>
2025-09-10 23:27:27 +0900

    Update home.js

--- src/home.js
+++ src/home.js
@@ -3,9 +3,16 @@ import "./home.css";
 
 const Home = () => {
   const [isOpened, setIsOpened] = useState(false);
+  const [showCard, setShowCard] = useState(false); // 카드 표시 상태
 
   const handleClick = () => {
     setIsOpened(true);
+    setShowCard(true);
+  };
+  
+  const handleCardClose = () => {
+    setShowCard(false);
+    setIsOpened(false);
   };
 
   return (
@@ -19,6 +26,19 @@ const Home = () => {
         onClick={handleClick}
         style={{ cursor: "pointer", marginTop: "20px" }}
       />
+
+      {showCard && (
+        <div className="card-container">
+          <img
+            src="/images/curation_card.png"
+            alt="큐레이션 카드"
+            className="curation-card"
+          />
+          <button className="card-close-button" onClick={handleCardClose}>
+            X
+          </button>
+        </div>
+      )}
     </div>
   );
 };
