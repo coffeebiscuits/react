9054b3f9037a0f67038954f8d1b71c180b6d1940
coffeebiscuits <opopkl-@nate.com>
2025-09-10 17:59:07 +0900

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 
 const Home = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleClick = () => {
+    setIsOpened(true);
+  };
+
   return (
     <div className="home-container">
       <h1>홈 페이지 테스트</h1>
       <p>버튼을 눌러 이동 성공!</p>
+      
+      <img
+        src={isOpened ? "/images/envelope_opened.png" : "/images/envelope_closed.png"}
+        alt="편지봉투"
+        className="envelope"
+        onClick={handleClick}
+        style={{ cursor: "pointer", width: "150px", marginTop: "20px" }}
+      />
     </div>
   );
 };
