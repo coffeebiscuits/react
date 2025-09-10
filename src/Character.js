aecaeb8b1e89c2c34622610c0ff1d2759ecc563d
coffeebiscuits <opopkl-@nate.com>
2025-09-10 13:42:02 +0900

    Update Character.js

--- src/Character.js
+++ src/Character.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+mport React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // 추가
 import "./Character.css";
 
